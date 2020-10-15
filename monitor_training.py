ac42ef1cc3961156feb0a0ddebd773cd278ba962
Jesse Desjardins <jesse.m.desjardins@gmail.com>
2020-10-15 17:43:44 -0400

    update demo files

--- monitor_training.py
+++ monitor_training.py
@@ -4,13 +4,13 @@ import gym
 import numpy as np
 import matplotlib.pyplot as plt
 
-from stable_baselines3 import DDPG, TD3
-from stable_baselines3.ddpg.policies import MlpPolicy
-from stable_baselines3.common import monitor
-from stable_baselines3.common.results_plotter import load_results, ts2xy
-from stable_baselines3.common.noise import AdaptiveParamNoiseSpec, NormalActionNoise
+from stable_baselines3 import TD3
+from stable_baselines3.common import results_plotter
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.results_plotter import load_results, ts2xy, plot_results
+from stable_baselines3.common.noise import NormalActionNoise
 from stable_baselines3.common.callbacks import BaseCallback
-from stable_baselines3 import results_plotter
+
 
 class SaveOnBestTrainingRewardCallback(BaseCallback):
     """
@@ -56,81 +56,24 @@ class SaveOnBestTrainingRewardCallback(BaseCallback):
 
         return True
 
-def moving_average(values, window):
-    """
-    Smooth values by doing a moving average
-    :param values: (numpy array)
-    :param window: (int)
-    :return: (numpy array)
-    """
-    weights = np.repeat(1.0, window) / window
-    return np.convolve(values, weights, 'valid')
-
-
-def plot_results(log_folder, title='Learning Curve'):
-    """
-    plot the results
-
-    :param log_folder: (str) the save location of the results to plot
-    :param title: (str) the title of the task to plot
-    """
-    x, y = ts2xy(load_results(log_folder), 'timesteps')
-    y = moving_average(y, window=50)
-    # Truncate x
-    x = x[len(x) - len(y):]
-
-    fig = plt.figure(title)
-    plt.plot(x, y)
-    plt.xlabel('Number of Timesteps')
-    plt.ylabel('Rewards')
-    plt.title(title + " Smoothed")
-    plt.show()
-
 # Create log dir
-log_dir = "/tmp/gym/"
+log_dir = "tmp/"
 os.makedirs(log_dir, exist_ok=True)
 
 # Create and wrap the environment
 env = gym.make('LunarLanderContinuous-v2')
-# Logs will be saved in log_dir/monitor.csv
-env = monitor(env, log_dir)
+env = Monitor(env, log_dir)
 
-# Add some param noise for exploration
-param_noise = AdaptiveParamNoiseSpec(initial_stddev=0.1, desired_action_stddev=0.1)
-# Create the callback: check every 1000 steps
-callback = SaveOnBestTrainingRewardCallback(check_freq=1000, log_dir=log_dir)
-# Because we use parameter noise, we should use a MlpPolicy with layer normalization
-model = DDPG(MlpPolicy, env, param_noise=param_noise, verbose=0)
-# Train the agent
-model.learn(total_timesteps=int(1e5), callback=callback)
-
-# Helper from the library
-results_plotter.plot_results([log_dir], 1e5, results_plotter.X_TIMESTEPS, "DDPG LunarLander")
-
-plot_results(log_dir)
-
-# Create log dir
-log_dir = "/tmp/gym/td3/"
-os.makedirs(log_dir, exist_ok=True)
-
-# Create and wrap the environment
-env = gym.make('LunarLanderContinuous-v2')
-# Logs will be saved in log_dir/monitor.csv
-env = monitor(env, log_dir)
-
-# Create action noise because TD3 and DDPG use a deterministic policy
+# Add some action noise for exploration
 n_actions = env.action_space.shape[-1]
 action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
-
-policy_kwargs = dict(layers=[400, 300])
-model_td3 = TD3("MlpPolicy", env, action_noise=action_noise, buffer_size=int(1e5), policy_kwargs=policy_kwargs, verbose=0)
-
+# Because we use parameter noise, we should use a MlpPolicy with layer normalization
+model = TD3('MlpPolicy', env, action_noise=action_noise, verbose=0)
 # Create the callback: check every 1000 steps
 callback = SaveOnBestTrainingRewardCallback(check_freq=1000, log_dir=log_dir)
-
 # Train the agent
-model_td3.learn(total_timesteps=int(1e5), callback=callback)
+timesteps = 1e5
+model.learn(total_timesteps=int(timesteps), callback=callback)
 
-plot_results(log_dir)
-
-results_plotter.plot_results([log_dir], 1e5, results_plotter.X_TIMESTEPS, "TD3 LunarLander")
+plot_results([log_dir], timesteps, results_plotter.X_TIMESTEPS, "TD3 LunarLander")
+plt.show()
